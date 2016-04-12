1ff90a3e44b0dd707994f819a856541bd5b6099d
Victorma Perez Colado <victorma-fire@hotmail.com>
2016-04-12 16:46:48 +0200

    Main Screen
    + Main screen
    + Fade in animation
    + Spin script
    + Start button

---
+++ Assets/SoftMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoftMovement : MonoBehaviour {
+
+	public float timeElapsed = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float spin = 30f * Mathf.Sin (timeElapsed);
+
+		timeElapsed += Time.deltaTime;
+
+		transform.localRotation = Quaternion.Euler (0, 0, spin);
+
+	}
+}
